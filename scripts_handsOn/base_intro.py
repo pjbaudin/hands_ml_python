5eb4880cc86356150946298588669748aca9d0f7
Pierre Baudin <pierrejean.baudin@hotmail.com>
2019-02-15 17:26:24 +0800

    create full pipeline to prep data, update notebook

--- scripts_handsOn/base_intro.py
+++ scripts_handsOn/base_intro.py
@@ -6,8 +6,9 @@ import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelBinarizer, StandardScaler
+from sklearn.pipeline import FeatureUnion
 
 PATH = "data/handson-ml-master/datasets/housing/housing.csv"
 
@@ -43,45 +44,80 @@ for set in (strat_train_set, strat_test_set):
 
 # Strategy for imputing missing value: use median
 housing = strat_train_set.drop("median_house_value", axis=1)
-housing_labesl = strat_train_set["median_house_value"].copy()
-
-imputer = SimpleImputer(strategy="median")
+housing_labels = strat_train_set["median_house_value"].copy()
 
 housing_num = housing.drop("ocean_proximity", axis=1)
-
-imputer.fit(housing_num)
-
-X = imputer.transform(housing_num)
-housing_tr = pd.DataFrame(X, columns=housing_num.columns)
-
-# Encoding categorical variables
-encoder_bin = LabelBinarizer()
-
-housing_cat = housing['ocean_proximity']
-housing_cat_1hotlab = encoder_bin.fit_transform(housing_cat)
 
 
 # Custom Transformers
 rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
 
+
 class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
-    def __init__(self, add_bedrooms_per_room = True):
+    def __init__(self, add_bedrooms_per_room=True):
         # no *args or **kwargs
         self.add_bedrooms_per_room = add_bedrooms_per_room
 
     def fit(self, X, y=None):
-        return self # nothing else to do
-    
+        return self  # nothing else to do
+
     def transform(self, X, y=None):
         rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
         population_per_household = X[:, population_ix] / X[:, household_ix]
         if self.add_bedrooms_per_room:
             bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
-            return np.c_[X, rooms_per_household, 
-                            population_per_household, bedrooms_per_room]
+            return np.c_[X, rooms_per_household,
+                         population_per_household, bedrooms_per_room]
         else:
             return np.c_[X, rooms_per_household, population_per_household]
-        
+
+
 attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
 housing_extra_attribs = attr_adder.transform(housing.values)
 
+
+# Class for selecting desired dataframe
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.attribute_names].values
+
+# class for improving BinaryLabelizer to work in Pipeline
+class ImprovedLabelBinarizer(TransformerMixin):
+    def __init__(self, *args, **kwargs):
+        self.encoder = LabelBinarizer(*args, **kwargs)
+    def fit(self, x, y=0):
+        self.encoder.fit(x)
+        return self
+    def transform(self, x, y=0):
+        return self.encoder.transform(x)
+
+# Feature Scaling and transformation pipelines
+# Complete pipeline including cat attributes
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+num_pipeline = Pipeline([
+    ('selector', DataFrameSelector(num_attribs)),
+    ('imputer', SimpleImputer(strategy="median")),
+    ('attribs_adder', CombinedAttributesAdder()),
+    ('std_scaler', StandardScaler()),
+])
+
+cat_pipeline = Pipeline([
+    ('selector', DataFrameSelector(cat_attribs)),
+    ('label_binarizer', ImprovedLabelBinarizer()),
+])
+
+full_pipeline = FeatureUnion(transformer_list=[
+    ("num_pipeline", num_pipeline),
+    ("cat_pipeline", cat_pipeline),
+])
+
+# Pipe the dataset for processing
+housing_prep = full_pipeline.fit_transform(housing)
